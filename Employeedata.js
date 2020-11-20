1757b31951382128c32278c990382b120b08fc59
authentictorch25 <64377971+authentictorch25@users.noreply.github.com>
2020-11-20 17:59:50 +0530

    [Akash] Added program to validate age and gender

--- Employeedata.js
+++ Employeedata.js
@@ -1,45 +1,45 @@
 class EmployeeData{
-// Property
-id;
-salary;
-gender;
-startDate;
+ // Property
+ id;
+ salary;
+ gender;
+ startDate;
 
-// constructor
-constructor(...params) {
-    this.id = params[0];
-    this.name = params[1];
-    this.salary = params[2];
-    this.gender = params[3];
-    this.startDate = params[4];
-}
+ // constructor
+ constructor(...params) {
+     this.id = params[0];
+     this.name = params[1];
+     this.salary = params[2];
+     this.gender = params[3];
+     this.startDate = params[4];
+ }
 
-// getter and setter method
-get name() { return this._name; }
-set name(name) {
-    let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-    if (nameRegex.test(name)) {
-        this._name = name;
-    } else {
-        throw 'Name is Incorrect!';
-    }
-}
+ // getter and setter method
+ get name() { return this._name; }
+ set name(name) {
+     let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+     if (nameRegex.test(name)) {
+         this._name = name;
+     } else {
+         throw 'Name is Incorrect!';
+     }
+ }
 
-// method
-toString() {
-    const options = { year: 'numeric', month: 'long', day: 'numeric' };
-    const empDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
-    return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", startDate = " + empDate;
-}
+ // method
+ toString() {
+     const options = { year: 'numeric', month: 'long', day: 'numeric' };
+     const empDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+     return "id = " + this.id + ", name = " + this.name + ", salary = " + this.salary + ", gender = " + this.gender + ", startDate = " + empDate;
+ }
 }
 
 let employeePayrollData = new EmployeeData(1, "Virat", 3000);
 console.log(employeePayrollData.toString());
 try {
-employeePayrollData.name = "rohit";
-console.log(employeePayrollData.toString());
+ employeePayrollData.name = "rohit";
+ console.log(employeePayrollData.toString());
 } catch (e) {
-console.error(e);
+ console.error(e);
 }
 let newEmployeePayrollData = new EmployeeData(1, "Smith", 3000, 'M', new Date());
 console.log(newEmployeePayrollData.toString());
